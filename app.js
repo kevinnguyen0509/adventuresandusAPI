af532b42323b51c86a67151e2afef8372bdee47a
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2021-05-28 12:10:42 -0700

    verison 1

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use(express.json());
+app.use(compression());
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
